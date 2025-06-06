96e4f9457d1562e634661f524f71f18d58eb8fae
Miken Thakur <mikenthakur7@gmail.com>
2025-06-06 08:59:06 +0545

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connect } from "mongoose"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 // app  config
@@ -14,6 +15,9 @@ app.use(cors())
 
 // db connection
 connectDB();
+
+// api endpoints 
+app.use("/api/food",foodRouter)
 
 
 app.get("/", (req,res)=> {
