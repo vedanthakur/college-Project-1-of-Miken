2f2dce048bf2cd770645509353d35f2184ac9a29
Miken Thakur <mikenthakur7@gmail.com>
2025-06-17 21:06:56 +0545

    updated

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -12,7 +12,15 @@ const addFood = async (req,res) => {
      description:req.body.description,
      price:req.body.price,
      category:req.body.category,
-
+     image:image_filename   
   })
+
+    try{
+        await food.save();
+        res.json({success:true,message:"Food Added"})
+    }catch (error){
+        console.log(error)
+        res.json({success:false,message:"Error"})
+    }
 }
 export {addFood}
