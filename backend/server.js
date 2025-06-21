02413b45bcf64a68f7968d002e691354bc6fd6f4
Miken Thakur <mikenthakur7@gmail.com>
2025-06-21 10:57:33 +0545

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors" // cross-origin resource sharing
 import { connect } from "mongoose"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
 
 
 // app  config
@@ -21,6 +22,7 @@ connectDB();
 // api endpoints 
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 
 app.get("/", (req,res)=> {
