4da19b6586a4d1d92f6a6b2514f4161fd5bcfcb9
Miken Thakur <mikenthakur7@gmail.com>
2025-06-21 10:39:40 +0545

    user auth(login & registration)

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+import userModel from "../modles/userModel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+
+// login user 
+const loginUser = async (req,res) => {
+
+
+}
+
